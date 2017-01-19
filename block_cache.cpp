00e544df3959832dffb949486170e241328229fa
Arvin Sharma <sharma.arv@husky.neu.edu>
2017-01-18 21:30:51 -0500

    baby steps

--- block_cache.cpp
+++ block_cache.cpp
@@ -10,32 +10,38 @@ int
 BlockCache::write(string path, const uint8_t* buf, uint64_t size, uint64_t offset) {
     // create blocks for buf. Number of blocks = ceil(size / block_size)
     uint64_t num_blocks = (size + Block::get_logical_size() - 1) / Block::get_logical_size();
+    bool in_cache = file_cache_.find(path) == file_cache_.end();
     // check if path has blocks at those indexes
-    if (file_cache_.find(path) == file_cache_.end()) {
-        // for each block, add to cache for file
-        uint64_t cached_bytes = 0;
-        uint64_t block_size = 0;
-        for (unsigned int block_count = 0; block_count < num_blocks; block_count++) {
+    // for each block, add to cache for file
+    uint64_t cached_bytes = 0;
+    uint64_t block_size = 0;
+    for (unsigned int block_count = 0; block_count < num_blocks; block_count++) {
 
-            // first jump to latest byte
-            cached_bytes += block_size;
+        // first jump to latest byte
+        cached_bytes += block_size;
 
-            // then determine how much space we have to add
-            if (cached_bytes + Block::get_logical_size() > size)
-                block_size = size - cached_bytes;
-            else
-                block_size = Block::get_logical_size();
+        // then determine how much space we have to add
+        if (cached_bytes + Block::get_logical_size() > size)
+            block_size = size - cached_bytes;
+        else
+            block_size = Block::get_logical_size();
 
-
-            // finally create block with that much space at the current byte
-            shared_ptr<Block> ptr(new Block(buf + cached_bytes, block_size));
-
-            // add newly formed block to file cache
-            file_cache_[path][offset + cached_bytes] = ptr;
+        if (in_cache) {
+            // invalidate old block
+            file_cache_[path][offset + cached_bytes]->set_dirty();
+            // release ownership
+            file_cache_[path][offset + cached_bytes].reset();
         }
 
+        // finally create block with that much space at the current byte
+        shared_ptr<Block> ptr(new Block(buf + cached_bytes, block_size));
 
+        // add newly formed block to file cache
+        file_cache_[path][offset + cached_bytes] = ptr;
     }
+
+
+
     // file_cache_.insert( buf to block))
     // if it doesnt then just add it and blocks
     // else
