7e09b8de63dae38b538c70504f77b3da2c168bc5
Arvin Sharma <sharma.arv@husky.neu.edu>
2017-01-19 10:56:14 -0500

    stuff

--- block_cache.cpp
+++ block_cache.cpp
@@ -19,7 +19,7 @@ BlockCache::write(string path, const uint8_t* buf, uint64_t size, uint64_t offse
     // for each block, add to cache for file
     uint64_t curr_idx = 0;
     uint64_t block_size = 0;
-    for (unsigned int block_idx = offset / Block::get_logical_size(), j = 0; j < num_blocks; block_idx++, j++) {
+    for (unsigned int block_idx = offset / Block::get_logical_size();  block_idx < num_blocks; block_idx++) {
         curr_idx += block_size;
         if (size < curr_idx + Block::get_logical_size())
             block_size = size - curr_idx;
