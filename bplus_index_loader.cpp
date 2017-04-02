dc72049c8adff47cd4eab92f647fd86247f38f36
Arvin Sharma <sharma.arv@husky.neu.edu>
2017-04-01 21:50:53 -0400

    starting loader

--- bplus_index_loader.cpp
+++ bplus_index_loader.cpp
@@ -14,8 +14,6 @@ BPLUSIndexLoader::load_trees() {
     struct dirent* entry;
     while ((entry = readdir(tree_dir))) {
         // check if valid tree file
-        //
-        //
         // mmap it to the unordered map
         string ent = entry->d_name;
         if (ent.substr(0,5).compare("TREE-") != 0)
@@ -41,4 +39,49 @@ BPLUSIndexLoader::load_trees() {
         // add pointer to map
         file_to_mem_[fpath] = fmem;
     }
+}
+
+BPLUSIndexLoader::disk_inode_info
+BPLUSIndexLoader::find_latest_inode(string path, bool get_data) {
+    load_trees();
+    cout << "finding inode for " << path << "getdata? " << to_string(get_data) << endl;
+    disk_inode_info latest;
+    latest.i_mtime = 0;
+    if (path.size() == 0)
+        return latest;
+
+    // create hash for path
+    char phash[128] = {'\0'};
+    string sthash = Util::crypto_hash(path);
+    memcpy(phash, sthash.c_str(), sthash.size());
+    cout << "PHASH: " << phash << endl;
+    uint64_t latest_time = 0;
+    for (auto tree : file_to_mem_) {
+        cout << "TREE: " << tree.first << endl;
+
+        // traverse thru tree
+        BPLUSIndex::node* cur = (BPLUSIndex::node*)tree.second;
+        for(;;) {
+            // check if this node contains this key
+            for (int64_t ii = 0; ii < cur->num_keys; ii++) {
+                // find this key
+                int64_t keyof = cur->keys[ii];
+                char* kmem = (char*)tree.second + keyof;
+
+                if (strcmp(kmem, phash) == 0) {
+                    // we found it
+                    if (cur->is_leaf)
+                        break;
+                    else {
+                        // its in a child
+                        int64_t childof = cur->children[ii + 1];
+                        cur = (BPLUSIndex::node*)((char*)tree.second + childof);
+                        continue;
+
+                    }
+                }
+            }
+        }
+    }
+    return latest;
 }
